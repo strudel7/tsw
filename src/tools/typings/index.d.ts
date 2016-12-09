8ab4b5f045682872c00d114d6914149ea2f8d66f
strudel <microsoft@strisys.com>
2016-12-09 14:18:54 -0500

    fixed typings.json to transpile to tools/typings

---
+++ src/tools/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/del/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/gulp/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
